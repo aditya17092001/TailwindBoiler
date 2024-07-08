11350259bc4cf4490eaefbedd1e692caf5909f9f
aditya17092001 <adityaudata@gmail.com>
2024-07-08 12:42:19 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center h-screen bg-black'>
+        <div className='flex justify-center bg-pink-500 border border-white'>
+          Hai
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
